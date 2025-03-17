e2ba047a478250d8add204315d73b61d2b741379
Mohnish <mohnish.a007@gmail.com>
2025-03-17 00:21:04 +0000

    First setup

---
+++ vizreact/src/App.jsx
@@ -0,0 +1,39 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className=''>
+      <svg width="960" height="500">
+        <circle 
+        cx="480" 
+        cy="250" 
+        fill="yellow" 
+        stroke="black"
+        stroke-width="5"
+        r="245">
+        </circle>
+
+        <circle 
+        cx="380" 
+        cy="150" 
+        r="50">
+        </circle>
+        <circle 
+        cx="580" 
+        cy="150" 
+        r="50">
+        </circle>
+    </svg>
+      </div>
+      <p className="read-the-docs">
+        Some data Viz 
+      </p>
+    </>
+  )
+}
+
+export default App
